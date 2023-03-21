d57d05932ee13def15ee70c6cd6d74749b54980f
Alhadi Kasyfi <alhadikasyfi1431@gmail.com>
2023-03-21 15:14:48 +0700

    first commit

---
+++ app/src/main/java/com/bakwanku/myshop/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bakwanku.myshop.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bakwanku.myshop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
